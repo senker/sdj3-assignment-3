ec6cdbed3fb1dec560a435c2bd464e8924ba6bf5
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-10-09 21:55:28 +0200

    adding validation rules

--- BlazorTodoApp/Domain/Model/Todo.cs
+++ BlazorTodoApp/Domain/Model/Todo.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain;
 
 public class Todo
 {
     public int Id { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
     public int OwnerId { get; set; }
+    
+    [Required, MaxLength(128)]
     public string Title { get; set; }
     public bool IsCompleted { get; set; }
 
@@ -11,5 +17,9 @@ public class Todo
     {
         OwnerId = ownerId;
         Title = title;
+    }
+
+    public Todo()
+    {
     }
 }
