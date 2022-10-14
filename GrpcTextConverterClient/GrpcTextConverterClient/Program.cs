b32d69996245170b6fc0913eb2461bf6d929d6c6
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-10-14 20:24:54 +0200

    adding grpc client in csharp

---
+++ GrpcTextConverterClient/GrpcTextConverterClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcTextConverterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:8084");
+var client = new TextConverter.TextConverterClient(channel);
+var reply = client.capitalizeFirstCharacter(
+    new RequestText() { InputText = "hello there"});
+Console.WriteLine("Greeting: " + reply);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
