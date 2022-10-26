b596904a2d972dd119330de6eecb087c478d3a8d
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-10-26 11:32:20 +0200

    Adding WebAPI controller

---
+++ TodoAppWasm/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserLogic userLogic;
+
+    public UsersController(IUserLogic userLogic)
+    {
+        this.userLogic = userLogic;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateAsync(UserCreationDto dto)
+    {
+        try
+        {
+            User user = await userLogic.CreateAsync(dto);
+            return Created($"/users/{user.Id}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
