7dca1d75d8febc5c46311de1592b0eb7f1760555
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-11-04 02:06:07 +0100

    feat: getAllProductFromAnimal

--- slaughterhouse-grpc-springboot/src/main/java/via/sdj3/slaughterhousegrpcspringboot/repositories/ProductPackRepository.java
+++ slaughterhouse-grpc-springboot/src/main/java/via/sdj3/slaughterhousegrpcspringboot/repositories/ProductPackRepository.java
@@ -4,5 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import via.sdj3.slaughterhousegrpcspringboot.models.ProductPackModel;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface ProductPackRepository extends JpaRepository<ProductPackModel, Long> { }
+public interface ProductPackRepository extends JpaRepository<ProductPackModel, Long> {
+//    List<ProductPackModel> findProductPackModelsBy
+//    Optional<List<ProductPackModel>> findProductPackModelBy
+}
